61abeab1f0db03250a23e81953127db9cb8c304e
Anatoli Babenia <anatoli@rainforce.org>
2018-02-01 14:17:50 +0300

    Add missing DIR declaration

--- up.chains.sh
+++ up.chains.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# show commands as they are executed
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export COMPOSE_FILE="$DIR/search/docker-compose.chains.yml"
 docker-compose pull
