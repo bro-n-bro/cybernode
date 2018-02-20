35e9ccaa802058ef86f813f8e1c1af69a4027995
Hleb Albau <hleb.albau@gmail.com>
2018-02-20 10:54:52 +0300

    Add repository repull for ethereum indexations

--- up.pump.ethereum.sh
+++ up.pump.ethereum.sh
@@ -12,11 +12,11 @@ cd "$CYBERNODE_REPOSITORY"
 export COMPOSE_FILE="$CYBERNODE_REPOSITORY/index/ethereum.yml"
 
 docker build -t local-build/pump-eth -f "$SEARCH_REPOSITORY/pumps/ethereum/Dockerfile" "$SEARCH_REPOSITORY"
-docker tag local-build/pump-eth local-build/pump-eth:latest
+docker tag local-build/pump-eth local-build/pump-ethereum:latest
 docker-compose stop pump-eth
 docker-compose run pump-eth
 
 docker build -t local-build/dump-eth -f "$SEARCH_REPOSITORY/dumps/ethereum/Dockerfile" "$SEARCH_REPOSITORY"
-docker tag local-build/dump-eth local-build/dump-eth:latest
+docker tag local-build/dump-eth local-build/dump-ethereum:latest
 docker-compose stop dump-eth
 docker-compose run dump-eth
