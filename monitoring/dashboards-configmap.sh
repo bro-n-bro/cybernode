aff86cf0f190dc12be763585d03b359d3f6de522
Valentin Stavetski <staveckij.v@gmail.com>
2018-04-14 17:16:48 +0300

    add configuration for deploy exchanges connector with metrics to gcloud

--- monitoring/dashboards-configmap.sh
+++ monitoring/dashboards-configmap.sh
@@ -5,3 +5,6 @@ kubectl delete configmap grafana-dashboards-etc --namespace=monitoring
 kubectl create configmap grafana-dashboards-common --from-file=dashboards/ --namespace=monitoring
 kubectl create configmap grafana-dashboards-eth --from-file=dashboards/ethereum --namespace=monitoring
 kubectl create configmap grafana-dashboards-etc --from-file=dashboards/ethereum_classic --namespace=monitoring
+
+kubectl delete configmap grafana-dashboards-exchanges --namespace=monitoring
+kubectl create configmap grafana-dashboards-exchanges --from-file=dashboards/markets --namespace=monitoring
