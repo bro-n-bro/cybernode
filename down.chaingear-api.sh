5324b1a6a2119a9f861b0a6258bc624e11bcc162
Hleb Albau <hleb.albau@gmail.com>
2018-04-18 15:24:14 +0300

    #24 Initial CLI: Move kubernetes definitions to subfolder
    --delete old compose definitions
    --move kube denitions to subfolder "kubernetes-definitions"

--- down.chaingear-api.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# show commands as they are executed
-set -x
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export COMPOSE_FILE="$DIR/chaingear-api/docker-compose.yml"
-docker-compose down
-
--- up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./up.browser.sh
-./up.chaingear-api.sh
-./up.ethereum.index.sh
-./up.search.api.sh
