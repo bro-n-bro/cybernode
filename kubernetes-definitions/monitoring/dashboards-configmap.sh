804bed06eff07bae6b3ff74c33e295f8492211c9
Hleb Albau <hleb.albau@gmail.com>
2018-06-21 17:27:07 +0300

    #90 Make chains monitoring templateable
    --add overall chains monitor
    --add bitcoin family monitor
    --add ethereum family monitor

--- kubernetes-definitions/monitoring/dashboards-configmap.sh
+++ kubernetes-definitions/monitoring/dashboards-configmap.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 kubectl delete configmap grafana-dashboards-common --namespace=monitoring
-kubectl delete configmap grafana-dashboards-eth --namespace=monitoring
-kubectl delete configmap grafana-dashboards-etc --namespace=monitoring
-kubectl create configmap grafana-dashboards-common --from-file=dashboards/ --namespace=monitoring
-kubectl create configmap grafana-dashboards-eth --from-file=dashboards/ethereum --namespace=monitoring
-kubectl create configmap grafana-dashboards-etc --from-file=dashboards/ethereum_classic --namespace=monitoring
-
-kubectl delete configmap grafana-dashboards-exchanges --namespace=monitoring
-kubectl create configmap grafana-dashboards-exchanges --from-file=dashboards/markets --namespace=monitoring
-
-kubectl delete configmap grafana-dashboards-btc --namespace=monitoring
-kubectl create configmap grafana-dashboards-btc --from-file=dashboards/bitcoin --namespace=monitoring
-
+kubectl delete configmap grafana-dashboards-chains --namespace=monitoring
 kubectl delete configmap grafana-dashboards-cluster --namespace=monitoring
+kubectl delete configmap grafana-dashboards-exchanges --namespace=monitoring
+
+
+
+kubectl create configmap grafana-dashboards-common --from-file=dashboards/ --namespace=monitoring
+kubectl create configmap grafana-dashboards-chains --from-file=dashboards/chains --namespace=monitoring
 kubectl create configmap grafana-dashboards-cluster --from-file=dashboards/cluster --namespace=monitoring
+kubectl create configmap grafana-dashboards-exchanges --from-file=dashboards/markets --namespace=monitoring
