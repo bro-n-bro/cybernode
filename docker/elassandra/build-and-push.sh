bf118cbe39d23599b588877278fb316b4bfca37f
Artur Albov <artur.albov@gmail.com>
2018-07-19 14:20:33 +0300

    Update elassandra to 5.5.0.20. New consumer group id for monitoring. Fix some requirements

--- docker/elassandra/build-and-push.sh
+++ docker/elassandra/build-and-push.sh
@@ -2,7 +2,7 @@
 # You must be logged in
 
 docker build -t build/elassandra .
-docker tag build/elassandra cybernode/elassandra:5.5.0.19
-docker push cybernode/elassandra:5.5.0.19
+docker tag build/elassandra cybernode/elassandra:5.5.0.20
+docker push cybernode/elassandra:5.5.0.20
 docker tag build/elassandra cybernode/elassandra:latest
 docker push cybernode/elassandra:latest
